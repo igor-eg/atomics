ab0bdb88ebf0bf635e17ef4512fe775bcf769e5c
Igor Egorov <igor73@list.ru>
2022-05-24 13:35:46 +0300

    Исправленная версия

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,34 @@
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.ExecutionException;
 import java.util.*;
 
 public class Main {
-    static LongAdder stat = new LongAdder();
     private static final int NUMBER_THREADS = 5;
 
     public static void main(String args[]) throws InterruptedException, ExecutionException {
-
+        LongAdder totalRevenueForTaxation = new LongAdder();
         Shops shop = new Shops();
         List<Shops> shops = new ArrayList<>();
+        List<List<Integer>> revenueResults = new ArrayList<>();
         ExecutorService ex = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
         for (int i = 0; i < NUMBER_THREADS; i++) {
             shops.add(new Shops());
+            Future<List<Integer>> revenueResult = ex.submit(shop);
+            revenueResults.add(revenueResult.get());
         }
 
-        String sum = ex.invokeAny(shops); // запускаем потоки и  выводим на печать общую сумму
-        System.out.println(sum);
+        for (List<Integer> entry : revenueResults) { // складываем выручку магазинов в LongAdder totalRevenueForTaxation
+            for (Integer each : entry)
+                totalRevenueForTaxation.add(each);
+        }
+
+        ex.shutdown();
+        System.out.println();
+        System.out.println("Общая выручка магазинов: " + totalRevenueForTaxation);
     }
-
 }
-
--- src/Shops.java
+++ src/Shops.java
@@ -3,26 +3,20 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-public class Shops implements Callable<String> {
+public class Shops implements Callable<List<Integer>> {
     private static final int NUMBER_OF_STORE_TRANSACTIONS = 5;
     private static final int THE_LOWER_BOUND_OF_THE_RANDOM_SALE_PRICE = 10;
     private static final int THE_LOWER_UPPER_OF_THE_RANDOM_SALE_PRICE = 100;
 
     @Override
-    public String call() throws Exception {
-
-
+    public List<Integer> call() throws Exception {
         List<Integer> intList = new Random().ints(NUMBER_OF_STORE_TRANSACTIONS,
                         THE_LOWER_BOUND_OF_THE_RANDOM_SALE_PRICE, THE_LOWER_UPPER_OF_THE_RANDOM_SALE_PRICE)
                 .boxed()
                 .collect(Collectors.toList());
-        for (Integer entry : intList) {
-            Main.stat.add(entry);
-
-        }
-        System.out.println(intList + " магазин " + Thread.currentThread().getName());
-
-        return "\nРезультат: " + Main.stat.sum();
+        System.out.printf("транзакции магазина %s следующие: %s", Thread.currentThread().getName(), intList);
+        System.out.println();
+        return intList;
     }
 }
 
